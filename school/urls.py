fcc91a726dedb8a44036832fb81b16777b6e7d7d
KruzhokProg <Aa1@3$5^7>
2021-12-10 09:32:51 +0300

    parameters get request

--- school/urls.py
+++ school/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path("answer_question/", views.AnswerQuestionView.as_view()),
     path("student/", views.StudentCreateView.as_view()),
     path("students_of_grade/<int:pk>", views.GradeStudentsDetailView.as_view()),
-    path("test/", views.GradeAPIView.as_view())
 ]
